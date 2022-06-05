71eb7829946f70b7975b215dc4caa0a835943538
abdulloh19 <parij4320@gmail.com>
2022-06-06 00:42:15 +0500

    tabs added

--- JavaScript.-43-dars.-Loader/Project Finish/js/script.js
+++ JavaScript.-43-dars.-Loader/Project Finish/js/script.js
@@ -4,6 +4,39 @@ window.addEventListener("DOMContentLoaded", () => {
     loader.style.opacity = "0";
     setTimeout(function () {
       loader.style.display = "none";
-    }, 2000);
-  }, 3500);
+    }, 1500);
+  }, 1000);
+
+  // TABS
+
+  const tabs = document.querySelectorAll(".tabheader__item");
+  const tabContent = document.querySelectorAll(".tabcontent");
+  const headParents = document.querySelector(".tabheader__items");
+
+  function hideTabContent () {
+    tabContent.forEach((item) => {
+      item.style.display = "none";
+    });
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active")
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabContent[i].style.display = "block";
+    tabs[i].classList.add("tabheader__item_active")
+  }
+  hideTabContent();
+  showTabContent();
+
+  headParents.addEventListener("click", (evt) => {
+    if (evt.target && evt.target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if(evt.target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      })
+    }
+  })
 });
